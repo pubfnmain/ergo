bfd287898c5fa9e41a1899abd3da10345c4ccf8c
pubfnmain <pubfnmain@gmail.com>
2025-06-02 14:56:47 +0600

    update: main loop

--- src/main.c
+++ src/main.c
@@ -10,14 +10,13 @@ main(int argc, char *argv[])
 {
 	struct state *state = state_init(argc, argv);
 	char input[BUFSIZ];
-	while (wl_display_flush(state->wl_display) != -1) { 
+	do { 
 		if (fgets(input, BUFSIZ, stdin)) {
 			parse_input(state, input);
 			wl_surface_attach(state->wl_surface, create_buffer(state), 0, 0);
 			wl_surface_damage_buffer(state->wl_surface, 0, 0, INT32_MAX, INT32_MAX);
 			wl_surface_commit(state->wl_surface);
 		}
-	}
-
+	} while (wl_display_dispatch(state->wl_display) != -1);
 	return 0;
 }
